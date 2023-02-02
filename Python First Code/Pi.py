830b87c944e9047ae279ba51b8f0ac40512ce0e9
Hıdır Güngör <hidirgungor@icloud.com>
2023-02-02 11:28:28 +0300

    code.hidirgungor.com
    code.hidirgungor.com

---
+++ Python First Code/Pi.py
@@ -0,0 +1,22 @@
+from vpython import *
+from random import random
+import math
+
+f1 = gcurve(color=color.green)
+f2 = gcurve(color=color.black)
+
+n = 0
+nc = 0
+while n<10000:
+    rate(1000)
+    x = random
+    y = random
+    r = math.sqrt(x**2 + y**2)
+    if r <= 1:
+        f1.plot(x,y)
+        nc = nc + 1
+    else:
+        f2.plot(x,y)
+    n = n + 1
+
+print("Pi = ", 4.*nc/n)
